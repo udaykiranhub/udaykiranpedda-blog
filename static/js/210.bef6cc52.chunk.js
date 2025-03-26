e16228acd3d3f78d694722b8763bb29dc2890694
udaykiranhub <peddaudaykiran6@gmail.com>
2025-03-26 06:05:19 +0530

    Updates

--- static/js/210.bef6cc52.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[210],{5210:(e,n,s)=>{s.r(n),s.d(n,{default:()=>i});var c=s(5043),l=s(579);const t=(0,c.lazy)((()=>s.e(671).then(s.bind(s,6671))));const i=function(){return(0,l.jsx)(l.Fragment,{children:(0,l.jsx)(c.Suspense,{fallback:(0,l.jsx)("div",{children:"Loading..."}),children:(0,l.jsx)(t,{})})})}}}]);
-//# sourceMappingURL=210.bef6cc52.chunk.js.map
